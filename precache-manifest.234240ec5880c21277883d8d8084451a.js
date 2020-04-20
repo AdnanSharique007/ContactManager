2c7f923d6f9cfe60b4cfd06ad87cc14efe33a7dc
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2020-04-21 02:47:58 +0530

    Updates

---
+++ precache-manifest.234240ec5880c21277883d8d8084451a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10d137d10ec1638f9d172ee8c6ae002",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "3f1aaf52a9372fcb1c15",
+    "url": "/ContactManager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0314c16556c28f425934",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3f1aaf52a9372fcb1c15",
+    "url": "/ContactManager/static/js/2.44be8b39.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.44be8b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0314c16556c28f425934",
+    "url": "/ContactManager/static/js/main.9e256150.chunk.js"
+  },
+  {
+    "revision": "8b7c913f2e5670d9d5dc",
+    "url": "/ContactManager/static/js/runtime-main.19cae20d.js"
+  }
+]);
